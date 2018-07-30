0647fa57688df9f5f22df3398737c4f66ece91ff
tavisca-asood <asood@tavisca.com>
2018-07-30 09:30:49 +0530

    Added Files

---
+++ BankSystem/Database/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.Database
+{
+    class Entity:Database.IDatabase
+    {
+        TrainingEntities Accounts = new TrainingEntities();
+
+        public void AddAccount(string fName, string lName, AccountType accountType, double balance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAccountType(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetBalance(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBalance(int ID, double balance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
